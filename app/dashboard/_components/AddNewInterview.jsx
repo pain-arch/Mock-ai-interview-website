df1383d9d571eb09f6331c98831f45efc5e05b12
pain-arch <himulol123@gmail.com>
2024-06-12 20:07:02 +0600

    feat: Add page for individual interviews and fetch interview details from the database

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -18,6 +18,7 @@ import { MockInterview } from "@/utils/schema";
 import { v4 as uuidv4 } from 'uuid';
 import { useUser } from "@clerk/nextjs";
 import moment from "moment";
+import { useRouter } from "next/navigation";
 
 function AddNewInterview() {
   const [openDialog, setOpenDialog] = useState(false);
@@ -27,6 +28,7 @@ function AddNewInterview() {
   const [loading, setLoading] = useState(false);
   const [jsonResponse, setJsonResponse] = useState([]);
   const { user } = useUser();
+  const router = useRouter();
 
   const onSubmit = async (e) => {
     setLoading(true);
@@ -56,6 +58,7 @@ function AddNewInterview() {
       console.log("Inserted ID:", resp);
       if (resp) {
         setOpenDialog(false);
+        router.push('/dashboard/interview/'+resp[0]?.mockId)
       }
     }
     else {
