833ba5af96ea64bf8c1fbc801c7154d732779b9e
pain-arch <himulol123@gmail.com>
2024-07-02 19:39:23 +0600

    feat: Completed the interview component for starting interview

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -4,6 +4,7 @@ import { db } from "@/utils/db";
 import { MockInterview } from "@/utils/schema";
 import { eq } from "drizzle-orm";
 import { Lightbulb, WebcamIcon } from "lucide-react";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Webcam from "react-webcam";
 
@@ -30,16 +31,29 @@ function Interview({ params }) {
       <h2 className="font-bold text-2xl">Let's Get Started</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-
         <div className="flex flex-col my-5 gap-5">
           <div className="flex flex-col p-5 rounded-lg border gap-5">
-            <h2 className="text-lg"><strong>Job Role/Job Position: </strong>{interviewData?.jobPositiopn}</h2>
-            <h2 className="text-lg"><strong>Job Description/Tech Stack: </strong>{interviewData?.jobDesc}</h2>
-            <h2 className="text-lg"><strong>Years of Experience: </strong>{interviewData?.jobExperience}</h2>
+            <h2 className="text-lg">
+              <strong>Job Role/Job Position: </strong>
+              {interviewData?.jobPositiopn}
+            </h2>
+            <h2 className="text-lg">
+              <strong>Job Description/Tech Stack: </strong>
+              {interviewData?.jobDesc}
+            </h2>
+            <h2 className="text-lg">
+              <strong>Years of Experience: </strong>
+              {interviewData?.jobExperience}
+            </h2>
           </div>
           <div className="p-5 border rounded-lg border-yellow-300 bg-yellow-100">
-            <h2 className="flex gap-2 items-center text-yellow-500"><Lightbulb /><strong>Information</strong></h2>
-            <h2 className="mt-3 text-yellow-600">{process.env.NEXT_PUBLIC_INFORMATION}</h2>
+            <h2 className="flex gap-2 items-center text-yellow-500">
+              <Lightbulb />
+              <strong>Information</strong>
+            </h2>
+            <h2 className="mt-3 text-yellow-600">
+              {process.env.NEXT_PUBLIC_INFORMATION}
+            </h2>
           </div>
         </div>
 
@@ -69,10 +83,12 @@ function Interview({ params }) {
             </>
           )}
         </div>
-
       </div>
       <div className="flex justify-end items-end">
-        <Button>Start Interview</Button>
+        <Link href={params.interviewId+"/start"}>
+          <Button>Start Interview</Button>
+        </Link>
+        <h1>{params.interviewId}</h1>
       </div>
     </div>
   );
---
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+
+function QuestionsSection({mockInterviewQuestion}) {
+  return (
+    <div className="p-5 border rounded-lg">
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+        {mockInterviewQuestion && mockInterviewQuestion.map((question, index) => (
+          <h2 className='p-2 bg-secondary rounded-full'>Question #{index+1}</h2>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default QuestionsSection
