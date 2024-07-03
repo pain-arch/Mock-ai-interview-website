aaf6d605c8cc25301ff1be9383b21a4fc0c430f8
pain-arch <himulol123@gmail.com>
2024-07-03 20:20:08 +0600

    feat: Refactor QuestionsSection component and improve UI/UX

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -1,16 +1,33 @@
-"use client"
-import React from 'react'
+"use client";
+import { Lightbulb } from "lucide-react";
+import React from "react";
 
-function QuestionsSection({mockInterviewQuestion}) {
-  return (
-    <div className="p-5 border rounded-lg">
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
-        {mockInterviewQuestion && mockInterviewQuestion.map((question, index) => (
-          <h2 className='p-2 bg-secondary rounded-full'>Question #{index+1}</h2>
-        ))}
+function QuestionsSection({ mockInterviewQuestion, activeQuestionIndex }) {
+  return mockInterviewQuestion&&(
+    <div className="p-5 border rounded-lg my-10">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {mockInterviewQuestion &&
+          mockInterviewQuestion?.map((question, index) => (
+            <h2
+              className={`p-2 border rounded-full text-xs md:text-sm text-center cursor-pointer ${
+                activeQuestionIndex == index && "text-white bg-primary"
+              }`}
+            >Question #{index + 1}</h2>
+          ))}
       </div>
+
+      <h2 className="my-5 text-md md:text-lg">{mockInterviewQuestion[activeQuestionIndex]?.Question}</h2>
+
+      <div className="border rounded-lg p-5 bg-blue-100 mt-20">
+        <h2 className="flex gap-2 items-center text-primary">
+          <Lightbulb />
+          <strong>Note: </strong>
+        </h2>
+        <h2 className="text-sm text-primary my-2">{process.env.NEXT_PUBLIC_INFORMATION}</h2>
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default QuestionsSection
+export default QuestionsSection;
