fe3d40c179daef51284d7fa836dd60310924dd4a
pain-arch <himulol123@gmail.com>
2024-07-07 17:26:41 +0600

    feat: Add text-to-speech functionality for interview questions

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -1,9 +1,20 @@
 "use client";
-import { Lightbulb } from "lucide-react";
+import { Lightbulb, Volume2 } from "lucide-react";
 import React from "react";
 
 function QuestionsSection({ mockInterviewQuestion, activeQuestionIndex }) {
-  return mockInterviewQuestion&&(
+  
+  const textToSpeech = (text) => {
+      if ('speechSynthesis' in window) {
+        const speech = new SpeechSynthesisUtterance(text);
+        window.speechSynthesis.speak(speech)
+      }
+      else {
+        alert("Sorry, your browser doesn't support text to speech!")
+      }
+  }
+
+  return mockInterviewQuestion && (
     <div className="p-5 border rounded-lg my-10">
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {mockInterviewQuestion &&
@@ -17,7 +28,7 @@ function QuestionsSection({ mockInterviewQuestion, activeQuestionIndex }) {
       </div>
 
       <h2 className="my-5 text-md md:text-lg">{mockInterviewQuestion[activeQuestionIndex]?.Question}</h2>
-
+      <Volume2 className="cursor-pointer" onClick={()=>textToSpeech(mockInterviewQuestion[activeQuestionIndex]?.Question)}/>
       <div className="border rounded-lg p-5 bg-blue-100 mt-20">
         <h2 className="flex gap-2 items-center text-primary">
           <Lightbulb />
