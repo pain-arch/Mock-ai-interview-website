0927c900d64c6633781ff1eb9ea708a91d0a5ffe
pain-arch <himulol123@gmail.com>
2024-07-10 13:52:15 +0600

    Refactor QuestionsSection component and improve UI/UX

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -27,8 +27,8 @@ function QuestionsSection({ mockInterviewQuestion, activeQuestionIndex }) {
           ))}
       </div>
 
-      <h2 className="my-5 text-md md:text-lg">{mockInterviewQuestion[activeQuestionIndex]?.Question}</h2>
-      <Volume2 className="cursor-pointer" onClick={()=>textToSpeech(mockInterviewQuestion[activeQuestionIndex]?.Question)}/>
+      <h2 className="my-5 text-md md:text-lg">{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
+      <Volume2 className="cursor-pointer" onClick={()=>textToSpeech(mockInterviewQuestion[activeQuestionIndex]?.question)}/>
       <div className="border rounded-lg p-5 bg-blue-100 mt-20">
         <h2 className="flex gap-2 items-center text-primary">
           <Lightbulb />
@@ -42,3 +42,41 @@ function QuestionsSection({ mockInterviewQuestion, activeQuestionIndex }) {
 }
 
 export default QuestionsSection;
+
+
+
+// const feedbackPrompt =
+//       "Question:" +
+//       mockInterviewQuestion[activeQuestionIndex]?.Question +
+//       "," +
+//       "Answer:" +
+//       userAnswer +
+//       ",Depends on question and user answer for give interview question " +
+//       " please give us rating for answer and feedback as area of improvement if any " +
+//       "in just 3 to 5 lines to improve it in JSON format with rating field and feedback feild";
+
+//     const result = await chatSession.sendMessage(feedbackPrompt);
+
+//     const mockJsonResponse = result.response
+//       .text()
+//       .replace("```json", "")
+//       .replace("```", "");
+//     console.log(mockJsonResponse);
+//     const JsonFeedbackResp = JSON.parse(mockJsonResponse);
+
+//     const resp = await db.insert(UserAnswer).values({
+//       mockIdRef: interviewData.mockId,
+//       question: mockInterviewQuestion[activeQuestionIndex]?.Question,
+//       correctAns: mockInterviewQuestion[activeQuestionIndex]?.Answer,
+//       userAns: userAnswer,
+//       feedback: JsonFeedbackResp?.feedback,
+//       rating: JsonFeedbackResp?.rating,
+//       userEmail: user?.primaryEmailAddress?.emailAddress,
+//       createdAt: moment().format("DD-MM-YYYY"),
+//     });
+
+//     if (resp) {
+//       toast("Your answer has been saved successfully");
+//     }
+//     setUserAnswer("");
+//     setLoading(false);
