6b247229a67421e971094c067477429deca883fa
pain-arch <himulol123@gmail.com>
2024-07-11 15:50:41 +0600

    feat: Refactor RecordAnsSection component and add speech-to-text functionality and send the data to the Neon database

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnsSection.jsx
@@ -1,13 +1,25 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Mic } from "lucide-react";
+import { db } from "@/utils/db";
+import { chatSession } from "@/utils/GeminiAiModal";
+import { UserAnswer } from "@/utils/schema";
+import { useUser } from "@clerk/nextjs";
+import { Mic, StopCircle } from "lucide-react";
+import moment from "moment";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import useSpeechToText from "react-hook-speech-to-text";
 import Webcam from "react-webcam";
+import { toast } from "sonner";
 
-function RecordAnsSection() {
+function RecordAnsSection({ activeQuestionIndex, interviewData, mockInterviewQuestion }) {
+
   const [userAnswer, setUserAnswer] = useState("");
+  const { user } = useUser();
+  const [loading, setLoading] = useState(false);
+
+  
+
   const {
     error,
     interimResult,
@@ -22,9 +34,69 @@ function RecordAnsSection() {
 
   useEffect(() => {
     results.map((result) => {
-      setUserAnswer(prevAns => prevAns +result?.transcript)
-     })
-  },[results])
+      setUserAnswer((prevAns) => prevAns + result?.transcript);
+    });
+  }, [results]);
+
+  useEffect(() => {
+    if (!isRecording&&userAnswer.length > 10) {
+      UpdateUserAnswer();
+    }
+  }, [userAnswer])
+
+  const StartStopRecording = async() => {
+    if (isRecording) {
+      
+      stopSpeechToText();
+      
+    } else {
+      startSpeechToText();
+    }
+  };
+
+
+  const UpdateUserAnswer = async () => {
+    console.log(userAnswer);
+    setLoading(true);
+
+    const feedbackPrompt =
+    "Question:" +
+    mockInterviewQuestion[activeQuestionIndex]?.Question +
+    "," +
+    "User Answer:" +
+    userAnswer +
+    ",Depends on question and user answer for give interview question " +
+    " please give us rating for answer and feedback as area of improvement if any " +
+    "in just 3 to 5 lines to improve it in JSON format with rating field and feedback feild";
+  
+    const result = await chatSession.sendMessage(feedbackPrompt);
+    const mockJsonResponse = (result.response.text().replace("```json", "").replace("```", ""));
+    
+    console.log(mockJsonResponse);
+
+    const JsonFeedbackResp = JSON.parse(mockJsonResponse);
+
+    const resp = await db.insert(UserAnswer).values({
+      mockIdRef: interviewData?.mockId,
+      question: mockInterviewQuestion[activeQuestionIndex]?.question,
+      correctAns: mockInterviewQuestion[activeQuestionIndex]?.answer,
+      userAns: userAnswer,
+      feedback: JsonFeedbackResp?.feedback,
+      rating: JsonFeedbackResp?.rating,
+      userEmail: user?.primaryEmailAddress?.emailAddress,
+      createdAt: moment().format("DD-MM-YYYY"),
+    })
+
+    if (resp) {
+      toast("Answer Saved Successfully");
+    } else {
+      toast("Error while saving answer, please record again");
+    }
+    setUserAnswer("");
+    setLoading(false);
+
+  }
+
 
   return (
     <div className="flex items-center justify-center flex-col">
@@ -44,14 +116,19 @@ function RecordAnsSection() {
           }}
         />
       </div>
-      <Button variant="outline" className="my-10"
-        onClick={isRecording ? stopSpeechToText : startSpeechToText}
-      >
-        {
-          isRecording ? <h2 className="text-red-600 flex gap-2"><Mic/>Stop Recording</h2> : "Record Answer"
-        } 
+      <Button disabled={loading} variant="outline" className="my-10" onClick={StartStopRecording}>
+        {isRecording ? (
+          <h2 className="text-red-600 animate-pulse flex gap-2 items-center">
+            <StopCircle />
+            Stop Recording
+          </h2>
+        ) : (
+          <h2 className="text-primary flex gap-2 items-center">
+          <Mic />
+          Record Answer
+        </h2>
+        )}
       </Button>
-      <Button onClick={()=>console.log(userAnswer)}>Show User Answer</Button>
     </div>
   );
 }
