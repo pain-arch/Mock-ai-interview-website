904a8e6870ba7524cee36ca1c9433a6941cfa523
pain-arch <himulol123@gmail.com>
2024-07-12 16:58:10 +0600

    feat: Add planData.jsx file for storing plan data

---
+++ utils/planData.jsx
@@ -0,0 +1,31 @@
+export default [
+    {
+        id: 0,
+        name: 'Free',
+        cost: 0,
+        offering: [
+            { value: "✅  Create 3 Free Mock Interview" },
+            { value: "✅  Unlimited Retake Interview" },
+            { value: "❌  Practice Question" },
+            { value: "❌  AIMock Exclusive App Access" },
+            { value: "❌  Email Support" },
+
+            
+        ]
+    },
+    {
+        id: 1,
+        name: 'Monthly',
+        cost: 5.99,
+        paymentLink: '',
+        offering: [
+            { value: "✅  Create 3 Free Mock Interview" },
+            { value: "✅  Unlimited Retake Interview" },
+            { value: "✅  Practice Question" },
+            { value: "✅  AIMock Exclusive App Access" },
+            { value: "✅  Email Support" },
+            
+            
+        ]
+    }
+]
